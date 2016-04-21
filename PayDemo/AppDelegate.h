cb5656b86c22ddb672ddb3b11927d13b5aa6a7b8
JustToBeBetter <li@li.local>
2016-04-21 15:34:51 +0800

    Initial Commit

---
+++ PayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PayDemo
+//
+//  Created by 李金柱 on 16/4/21.
+//  Copyright © 2016年 likeme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayDemo
+//
+//  Created by 李金柱 on 16/4/21.
+//  Copyright © 2016年 likeme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
